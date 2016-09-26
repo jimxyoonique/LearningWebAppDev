99bca520923b8431fe58f2fe7e43116b709e8874
Jimmy Yoon <jimmymaster22@csu.fullerton.edu>
2016-09-25 21:03:58 -0700

    Added part 2 of the hw

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,25 +1,46 @@
 var main = function () {
     "use strict";
 
-    var addCommentFromInputBox = function () {
+    var addCommentFromInputBox = function (x) {
         var $new_comment;
 
-        if ($(".comment-input input").val() !== "") {
-            $new_comment = $("<p>").text($(".comment-input input").val());
-            $new_comment.hide();
-            $(".comments").append($new_comment);
-            $new_comment.fadeIn();
-            $(".comment-input input").val("");
-        }
+        if (x == ".comment-input input") {
+         if ($(".comment-input input").val() !== "") {
+             $new_comment = $("<p>").text($(".comment-input input").val());
+             $new_comment.hide();
+             $(".comments").append($new_comment);
+             $new_comment.fadeIn();
+             $(".comment-input input").val("");
+           }
+         }
+
+        else if(x == ".comment-input1 input")
+         if ($(".comment-input1 input").val() !== "") {
+             $new_comment = $("<p>").text($(".comment-input1 input").val());
+             $new_comment.hide();
+             $(".comments1").append($new_comment);
+             $new_comment.fadeIn();
+             $(".comment-input1 input").val("");
+           }
     };
 
     $(".comment-input button").on("click", function (event) {
-        addCommentFromInputBox();
+        addCommentFromInputBox(".comment-input button");
     });
 
     $(".comment-input input").on("keypress", function (event) {
         if (event.keyCode === 13) {
-            addCommentFromInputBox();
+            addCommentFromInputBox(".comment-input input");
+        }
+    });
+
+    $(".comment-input1 button").on("click", function (event) {
+        addCommentFromInputBox(".comment-input1 button");
+    });
+
+    $(".comment-input1 input").on("keypress", function (event) {
+        if (event.keyCode === 13) {
+            addCommentFromInputBox(".comment-input1 input");
         }
     });
 };
